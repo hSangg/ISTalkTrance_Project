35c5a8d018be4b161cc68c838c70ded81c5d2d32
hoai sang <hoai.sang050@gmail.com>
2025-03-02 23:35:39 +0700

    method for label

---
+++ train/extract.py
@@ -0,0 +1,26 @@
+from pyannote.audio import Pipeline
+from datetime import timedelta
+import torchaudio
+
+pipeline = Pipeline.from_pretrained(
+    "pyannote/speaker-diarization-3.1",
+    use_auth_token="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")
+
+# # send pipeline to GPU (when available)
+# import torch
+# pipeline.to(torch.device("cuda"),)
+
+# apply pretrained pipeline
+diarization = pipeline("wav_videos/raw.WAV")
+
+with open("diarization_output.txt", "w") as file:
+    for turn, _, speaker in diarization.itertracks(yield_label=True):
+        start_time = str(timedelta(seconds=int(turn.start)))
+        end_time = str(timedelta(seconds=int(turn.end)))
+        result_line = f"{start_time} {end_time} {speaker}\n"
+        
+        # Print to console
+        print(result_line.strip())
+        
+        # Write to file
+        file.write(result_line)
